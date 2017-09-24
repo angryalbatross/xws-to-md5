ad75f892a71ee7a26aff53c23669482093b64f14
Dan Burgett <daniel.burgett@gmail.com>
2017-09-24 18:32:30 -0500

    Modified to translate xws raw json posts

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var md5 = require('md5');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+// create application/json parser 
+var jsonParser = bodyParser.json()
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("XWS2 app is running at localhost:" + app.get('port'))
 })
+
+// POST / gets urlencoded bodies 
+app.post('/', jsonParser, function (req, res) {
+	if (!req.body) return res.sendStatus(400)
+
+	var pilotNames = ""
+	var list = req.body
+
+	for (var i = 0, len = list.pilots.length; i < len; i++) {
+      pilotNames = pilotNames + list.pilots[i].name + ',';
+    }
+
+	res.send('Found Pilots = ' + pilotNames + ' Hashed = ' + md5(pilotNames))
+
+})
+
