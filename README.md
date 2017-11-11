bda9dbcaa835774d0d1e3fdff6eec14b5b244727
Lyle Hayhurst <sozinsky@gmail.com>
2017-11-11 10:57:12 -0800

    implemented postgres, updated README to reflect new routes

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ The service supports to calls: users can POST a json xws document to the service
 
 ### POST with XWS payload
 * **URL**
-  /api/v1/id
+  /api/v1/xws
 * **Method:**
   `POST`
 * **Success Response:**
   * **Code:** 200 <br />
-    **Content:** `{ id : "MD5 Identifier" }`
+    **Content:** `{ status: "success", id : "MD5 Identifier" }`
 * **Error Response:**
   * **Code:** 400 <br />
     **Content:** `{code: 1, error: 'json not found'  }`
@@ -22,7 +22,7 @@ The service supports to calls: users can POST a json xws document to the service
 * **Sample Call:**
 
     curl --request POST \
-      --url http://localhost:5000/api/v1/id \
+      --url http://localhost:5000/api/v1/xws \
       --header 'content-type: application/json' \
       --data '  {
     					"faction": "imperial",
@@ -85,7 +85,7 @@ The service supports to calls: users can POST a json xws document to the service
     None
 ### GET with MD5 identifer
 * **URL**
-  /api/v1/$id
+  /api/v1/id/$id
 * **Method:**
   `GET`
 *  **URL Params** 
@@ -98,7 +98,7 @@ The service supports to calls: users can POST a json xws document to the service
   * **Code:** 400 Identifier not found <br />
     **Content:** `{error: 'Could not find identifier provided'  }`
 * **Sample Call:**
->>http://localhost:5000/api/v1/41e92654aa3426db3fb199f0dff05df1 
+>>http://localhost:5000/api/v1/id/41e92654aa3426db3fb199f0dff05df1 
 
 ## Running Locally
 
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var md5 = require('md5');
 var app = express()
+
+app.use(bodyParser.json())
+
+var router = express.Router();
+var db = require("./queries")
+router.get('/api/v1/id/:id', db.getAList)
+router.post('/api/v1/xws', db.createList)
+router.get('/lists', db.getAllLists)
+app.use(router)
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -9,65 +17,16 @@ app.use(express.static(__dirname + '/public'))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-// create application/json parser 
-var jsonParser = bodyParser.json()
-var cool = require('cool-ascii-faces')
-
-//postgres
-var pg = require('pg');
-
 app.listen(app.get('port'), function() {
-  console.log("XWS2 app is running at localhost:" + app.get('port'))
+    console.log("XWS2 app is running at localhost:" + app.get('port'))
 })
 
-function isEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            return false;
-    }
-
-    return true;
-}
-
-//see README.md for endpoint behavior.
-app.post('/api/v1/id', jsonParser, function (req, res) {
-	if (!req.body || req.body.length == 0 || isEmpty(req.body)) return res.status(400).send( { code: 1, error : "json not found" } )
-
-	var pilotNames = ""
-	var list = req.body
-
-	for (var i = 0, len = list.pilots.length; i < len; i++) {
-      pilotNames = pilotNames + list.pilots[i].name + ',';
-    }
-
-    var md5 = md5(pilotNames)
-
-    //check to see if its already in the database
-
-	res.send( { 'id': md5(pilotNames) } )
-})
-
-//see README.md for endpoint behavior
-app.get('/api/v1/:listId', function (req, res) {
-	console.log(req.params["listId"])
-	if (!req.query.listId) return res.sendStatus(400)
-	res.send({ 'id': req.query.listId } )
-})
+// create application/json parser 
+var cool = require('cool-ascii-faces')
 
 app.get('/', function(request, response) {
     response.send(cool())
 })
 
-app.get('/view', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM ulid ', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/xws', {results: result.rows} ); }
-        });
-    });
-});
 
 
